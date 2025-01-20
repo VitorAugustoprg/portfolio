78c01daeecd75dc4df9e42d545ee30f600602b34
Vitor augusto de oliveira souza <108297780+VitorAugustoprg@users.noreply.github.com>
2025-01-20 20:22:59 -0300

    Seção sobre mim
    desenvolvimento inicial da seção

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,13 +6,17 @@ document.getElementById('menu-toggle').addEventListener('click', function(e) {
 
     if(menuContent.style.display === 'block'){
         menuContent.style.display = 'none';
+        menuContent.classList.remove('show');
         iconMenu.src = 'assets/icons/icons8-cardápio-50 1.png'
 
     } else{
         menuContent.style.display = 'block';
+        menuContent.classList.add('show');
         iconMenu.src = 'assets/icons/icons8-excluir-50 1.png'
 
     }
+
+ 
 
 
 });
